ec04d15d04f0464c3e718aedf297f0f0df5114bd
Tolik <talibasya@gmail.com>
2016-11-19 17:52:14 +0100

    4:3:4 add oauth via fb

--- rest-server-passport/config.js
+++ rest-server-passport/config.js
@@ -1,4 +1,9 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl' : 'mongodb://localhost:27017/conFusion'
+    'mongoUrl' : 'mongodb://localhost:27017/conFusion',
+    'facebook': {
+        clientID: '545970795604224',
+        clientSecret: 'f58cb1aa8c34b8562ad8f2a1b238b4a2',
+        callbackURL: 'https://localhost:3443/users/facebook/callback'
+    }
 }
